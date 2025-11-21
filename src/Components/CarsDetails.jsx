1668bad59234b3bdb5ab713d19da8985ca2c61db
Mi Akash <mszharul1990@gmail.com>
2025-11-21 16:54:16 +0600

    all fixed

--- src/Components/CarsDetails.jsx
+++ src/Components/CarsDetails.jsx
@@ -205,7 +205,7 @@ const CarsDetails = () => {
             ) : (
               <button
                 onClick={() => handleCancleBook(id)}
-                className="bg-zinc-100 rounded-full text-sm text-white py-2 w-full"
+                className="bg-red-400 rounded-full text-sm text-white py-2 w-full"
               >
                 Cancle Booking
               </button>
